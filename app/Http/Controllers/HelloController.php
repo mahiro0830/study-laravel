f7da8922874f102efb34db61fcf037d2fbfffc6d
峯岸真弘 <shishisus4@gmail.com>
2023-10-15 23:59:42 +0900

    Routeインスタンスのname()を使用してみる

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,4 +14,9 @@ class HelloController extends Controller
 
         return view('hello.index', $data);
     }
+
+    public function other() :object
+    {
+        return redirect()->route('hello');
+    }
 }
