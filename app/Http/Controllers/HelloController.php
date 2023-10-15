9caeb6b2e1cd64770fae1b91092b05e0186410ef
峯岸真弘 <shishisus4@gmail.com>
2023-10-16 04:16:52 +0900

    サービスプロバイダより、RouteのpersonパラメータにPersonモデルクラスを自動割当をする

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index( Person $person ) : object
+    public function index( $person ) : object
     {
         $data = [
             'msg' => $person,
